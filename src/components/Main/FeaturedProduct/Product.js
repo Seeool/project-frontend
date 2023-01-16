c5d6868d9e2c2a7088023a40cc8701e5fc9037a2
Seeool <news4214@protonmail.com>
2023-01-16 16:57:51 +0900

    23/01/16 장바구니 담기 버튼추가, 담기 완료를 알려줄 무언가가 필요함(ex모달)

--- src/components/Main/FeaturedProduct/Product.js
+++ src/components/Main/FeaturedProduct/Product.js
@@ -1,28 +1,39 @@
-import React, {useEffect, useRef} from 'react';
-import {Link} from "react-router-dom";
+import React, {useEffect, useRef, useState} from 'react';
 
 const Product = (props) => {
+
     const {type, name, price, picUrl} = props.product
+    const handleShow = props.handleShow
+    const addToCart = () => {
+        handleShow()
+
+    }
 
     return (
-        <div className={type+" col-lg-3 col-md-4 col-sm-6 mix"}>
-            <div className="featured__item">
-                <div
-                    className="featured__item__pic set-bg"
-                    data-setbg={picUrl}
-                >
-                    <ul className="featured__item__pic__hover">
-                        <li>
-                            <a href="src/components#"><i className="fa fa-shopping-cart"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="featured__item__text">
-                    <h6><a href="src/components#">{props.name}</a></h6>
-                    <h5>{price}</h5>
+        <>
+            <div className={type + " col-lg-3 col-md-4 col-sm-6 mix"}>
+                <div className="featured__item">
+                    <div
+                        className="featured__item__pic set-bg"
+                        data-setbg={picUrl}
+                    >
+                        <ul className="featured__item__pic__hover">
+                            <li>
+                                <a className="addCartBtn" href="src/components#"  >
+                                    <i className="fa fa-shopping-cart"></i>
+                                    <input type={'hidden'} value={name}/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="featured__item__text">
+                        <h6><a href="src/components#">{props.name}</a></h6>
+                        <h5>{price}</h5>
+                    </div>
                 </div>
             </div>
-        </div>
+
+        </>
     );
 };
 
